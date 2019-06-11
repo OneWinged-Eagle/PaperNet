a18b2fd7b9c2fa43607542c2732dd00669cbe961
Thuwa77 <guillaume.szwankowski@epitech.eu>
2019-06-11 15:57:43 +0900

    added issue and buy

--- commercial-paper/server.js
+++ commercial-paper/server.js
@@ -1,10 +1,28 @@
 const express = require("express")
+const issue = require("./organization/magnetocorp/application/issue")
+const buy = require("./organization/digibank/application/buy")
 const redeem = require("./organization/digibank/application/redeem")
 
 const app = express()
 const port = 3000
 
 app.get("/", (_req, res) => res.status(200).send("Hello World!"))
+
+app.get("/issue", (_req, res) => {
+	issue().then(() => {
+		res.send("Issue program complete.")
+	}).catch((err) => {
+		res.send(`Issue program exception: ${err}`)
+	})
+})
+
+app.get("/buy", (_req, res) => {
+	buy().then(() => {
+		res.send("Buy program complete.")
+	}).catch((err) => {
+		res.send(`Buy program exception: ${err}`)
+	})
+})
 
 app.get("/redeem", (_req, res) => {
 	redeem().then(() => {
