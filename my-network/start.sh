2482a3a87c49341558e3c4c335e25676fff592aa
OneWinged-Eagle <guillaume.szwankowski@epitech.eu>
2019-06-17 00:07:26 +0900

    yaml

--- my-network/start.sh
+++ my-network/start.sh
@@ -6,9 +6,9 @@ set -ev
 # don't rewrite paths for Windows Git Bash users
 export MSYS_NO_PATHCONV=1
 
-docker-compose -f docker-compose.yml down
+docker-compose -f docker-compose.yaml down
 
-docker-compose -f docker-compose.yml up -d
+docker-compose -f docker-compose.yaml up -d
 docker ps -a
 
 # wait for Hyperledger Fabric to start
